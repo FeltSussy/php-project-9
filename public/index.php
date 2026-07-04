14cc1facfe1eeec908132d0aa61b86ccfc86e795
Felt <icebadass@gmail.com>
2026-07-04 22:24:02 +1000

    add initial slim routing and phtm template with bootstrap

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo 'Hello!';
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use DI\Container;
+
+session_start();
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+$router = $app->getRouteCollector()->getRouteParser();
+
+$app->get('/', function($request, $response) {
+    return $this->get('renderer')->render($response, 'main.phtml');
+});
+
+$app->run();
