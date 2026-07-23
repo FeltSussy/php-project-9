5dd95b10ef180e0fb3a310958c607d2a5f1741a8
Pavel <icebadass@gmail.com>
2026-07-23 12:05:16 +1000

    check

--- src/Service/UrlCheckService.php
+++ src/Service/UrlCheckService.php
@@ -33,7 +33,6 @@ class UrlCheckService
         $url = $this->urlRepository->findById($urlId);
         try {
             $response = $this->client->get($url->getName(), ['timeout' => 15]);
-            error_log('Status: ' . $response->getStatusCode());
         } catch (ConnectException | ServerException $e) {
             return [
                 'status' => 'connect_failed',
